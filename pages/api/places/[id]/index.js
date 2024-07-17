1df6efdb71398b8c3b6c1c1fdf91a8bbd0a67d68
stepris <122841614+stepris@users.noreply.github.com>
2024-07-17 11:34:25 +0200

    Set up database and update api endpoints

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,17 +1,30 @@
-import { places } from "../../../../lib/db.js";
+// import { places } from "../../../../lib/db.js";
+import dbConnect from "@/db/connect";
+import Place from "@/db/models/Place";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
+
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+
+    if (!place) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+
+    response.status(200).json(place);
+  }
 
   if (!id) {
     return;
   }
 
-  const place = places.find((place) => place.id === id);
+  // const place = places.find((place) => place.id === id);
 
   if (!place) {
     return response.status(404).json({ status: "Not found" });
   }
 
-  response.status(200).json(place);
+  // response.status(200).json(place);
 }
